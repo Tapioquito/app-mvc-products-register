a26b17220b1683a2bc9fc38be62f0263f0d3a209
Caio Porto <caioportoramos@gmail.com>
2023-06-16 20:36:18 -0300

    initial push

---
+++ AppMvcProductRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppMvcProductRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
